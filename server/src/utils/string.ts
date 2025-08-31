245fdd560ea84feffbe0b9e1dbc196898de9a57c
Nils-dev-Mertens <nilsm2950@gmail.com>
2025-08-31 17:21:15 +0200

    testing standard doc generator

--- server/src/utils/string.ts
+++ server/src/utils/string.ts
@@ -1,3 +1,8 @@
+/**
+ * d
+ * @param l d
+ * @returns d
+ */
 export function generateRandomString(l:number):string{
     let r:string = "";
     const c:string[] = [..."qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"];
